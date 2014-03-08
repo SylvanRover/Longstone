aa13fee6f7c02c20f76f42be2932e3fb008e0278
SylvanRover <contact@denmanrooke.com>
2014-03-08 18:49:43 +0000

    first commit
    first commit

---
+++ Assets/JumpScriptAdv.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScriptAdv : MonoBehaviour 
+{
+	public bool canJump = true;
+	
+	void Update()		
+	{		
+		if (Input.GetKeyDown (KeyCode.Space) && canJump == true)
+		{
+			rigidbody.AddForce (0, 225, 0);	
+			canJump = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		foreach (ContactPoint contact in collision.contacts) 
+		{
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+
+		if (collision.relativeVelocity.magnitude > 0) 
+		{
+			canJump = true;
+			Debug.Log("Player is colliding");
+		}
+		
+	}
+}
