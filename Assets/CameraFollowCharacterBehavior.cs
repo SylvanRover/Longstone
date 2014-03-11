c9ae9b34dba3d4b18bce5ffe8b0d692ddafb49b3
SylvanRover <contact@denmanrooke.com>
2014-03-11 00:47:40 +0000

    came lerp

--- Assets/CameraFollowCharacterBehavior.cs
+++ Assets/CameraFollowCharacterBehavior.cs
@@ -8,20 +8,26 @@ public class CameraFollowCharacterBehavior : MonoBehaviour
 	public float cameraY = 2f;
 	public float cameraZ = -10f;
 	public Character3DmovementNoMoveOnJump _isGrounded;
+	public float cameraTweenTime = 1.0f;
 
 	// Use this for initialization
 	void Start () 
 	{
 	
 	}
+
+	IEnumerator TransisitonCamera ()
+	{
+		if (_isGrounded.isGrounded && _Player.rigidbody.velocity.y > 0) 
+		{
+			this.transform.position = Vector3.Lerp (this.transform.position, new Vector3 (this.transform.position.x, _Player.transform.position.y + cameraY, this.transform.position.z), cameraTweenTime * Time.deltaTime);
+			yield return null;
+		}
+	}
 	
 	void Update () 
 	{
-		Debug.Log ("this is working");
 		this.transform.position = new Vector3 (_Player.transform.position.x, this.transform.position.y, _Player.transform.position.z + cameraZ);
-		if (_isGrounded.isGrounded) 
-		{
-			this.transform.position = new Vector3 (this.transform.position.x, _Player.transform.position.y + cameraY, this.transform.position.z);
-		}
+		StartCoroutine (TransisitonCamera ());
 	}
 }
