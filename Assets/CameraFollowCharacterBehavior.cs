a9f7b5dddc7fc4e5784c16966e8c517ec41405b1
SylvanRover <contact@denmanrooke.com>
2014-03-12 00:27:22 +0000

    Fog & Transparent Elements between the Player

--- Assets/CameraFollowCharacterBehavior.cs
+++ Assets/CameraFollowCharacterBehavior.cs
@@ -9,37 +9,23 @@ public class CameraFollowCharacterBehavior : MonoBehaviour
 	public float cameraZ = -10f;
 	public Character3DmovementNoMoveOnJump _isGrounded;
 	public float cameraTweenTime = 1.0f;
-	public Transform startMarker;
-	public Transform endMarker;
-	public float speed = 1.0F;
+	public float cameraSpeed = 0.01F;
 	private float startTime;
 	private float journeyLength;
-	public Transform target;
-	public float smooth = 5.0F;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		startTime = Time.time;
-		journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+		startTime = Time.deltaTime;
+		journeyLength = Vector3.Distance(this.transform.position, new Vector3 (this.transform.position.x, _Player.transform.position.y + cameraY));
 	}
-	
-	/*IEnumerator TransisitonCamera ()
-	{
-		if (_isGrounded.isGrounded) 
-		{
-			this.transform.position = Vector3.Lerp (this.transform.position, new Vector3 (this.transform.position.x, _Player.transform.position.y + cameraY, this.transform.position.z), cameraTweenTime * Time.deltaTime);
-			yield return null;
-		}
-	}*/
 	
 	void Update () 
 	{
 		this.transform.position = new Vector3 (_Player.transform.position.x, this.transform.position.y, _Player.transform.position.z + cameraZ);
 		if (_isGrounded.isGrounded) 
 		{
-			//StartCoroutine (TransisitonCamera ());
-			float distCovered = (Time.time - startTime) * speed;
+			float distCovered = (Time.time - startTime) * cameraSpeed;
 			float fracJourney = distCovered / journeyLength;
 			transform.position = Vector3.Lerp (this.transform.position, new Vector3 (this.transform.position.x, _Player.transform.position.y + cameraY, this.transform.position.z), fracJourney);
 		}
