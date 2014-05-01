66492df2060d040f4e1369e98f629298b6f148af
SylvanRover <contact@denmanrooke.com>
2014-05-02 00:46:03 +0100

    Attempting to setup Parent triggering

---
+++ Assets/Scripts/C#/GateAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateAnimation : MonoBehaviour
+{
+	public GateTrigger gate;
+
+	private PlayerInventory playerInventory;
+	private HashIDs hash;
+	private GameObject player;
+	private Animator anim;
+
+	void Update ()
+	{
+		if (gate.gateTrigger)
+		{
+			anim.SetBool ("Open", true);
+		}
+	}
+}
